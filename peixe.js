13f69da9b80392dc831aa2b5561ff71f93ca904e
AdrianaPolicia <adriana.policia@sou.inteli.edu.br>
2025-02-05 16:28:55 -0300

    versao 1

---
+++ peixe.js
@@ -0,0 +1,43 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var peixinho;
+
+function preload() {
+    this.load.image('mar', 'assets/bg_azul-escuro.png');
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
+    this.load.image('peixe', 'assets/peixes/tubarao.png');
+    this.load.image('corais', 'assets/coralmar.png');
+}
+
+
+function create() {
+    this.add.image(400, 300, 'mar');
+    this.add.image(400, 100, 'logo').setScale(0.5);
+    this.add.image(400, 440, 'corais').setScale(1);
+
+    //guardar o peixe em uma variável
+    peixinho = this.add.image(400, 300, 'peixe');
+    
+    //transformando variável
+    peixinho.setFlip(true, false);
+}
+
+
+function update() {
+
+    //adicionando controles no peixe
+    peixinho.x = this.input.x;
+    peixinho.y = this.input.y;
+}
